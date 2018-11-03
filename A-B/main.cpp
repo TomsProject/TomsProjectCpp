5b869c41bf17ed0df843834edd40db1bddd3c5bc
TomsProject <44722363+TomsProject@users.noreply.github.com>
2018-11-03 19:21:54 +0800

    Update main.cpp

--- A-B/main.cpp
+++ A-B/main.cpp
@@ -4,9 +4,9 @@
   using namespace std;
   int main() {
   int x;
-  cout<<"Please input the number of printed documents.：";
+  cout<<"Please input the number of printed documents：";
   cin >> x;
-  cout << "A printing factory will receive" << 0.2 * x + 500 << "yuan！" << endl;
-  cout << "B printing factory will receive" << 0.4 * x << "yuan！" << endl;  
+  cout << "A printing factory will receive:" << 0.2 * x + 500 << "yuan！" << endl;
+  cout << "B printing factory will receive:" << 0.4 * x << "yuan！" << endl;  
   return 0;
 }
