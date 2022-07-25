7c54867465b89e9bff85c0a110bc57b034ca42d8
projectOMS <64317586+yxtp@users.noreply.github.com>
2022-07-25 17:06:27 +0800

    Update main.cpp

--- X+Y/main.cpp
+++ X+Y/main.cpp
@@ -1,10 +1,10 @@
-//THIS IS EASYEST IN THE WORLD
+
 #include <iostream>
 using namespace std;
 int main()
 {
-double a,b;
-cin>>a>>b;
-cout<<a+b<<endl;
-return 0;
+  double a, b;
+  cin >>  a >>  b;
+  cout  <<  a + b <<  endl;
+  return 0;
 }
